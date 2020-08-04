13e8553ab7aec53f9587cd403e89574186c000d6
Brad <bsller74@gmail.com>
2020-08-04 13:39:45 -0400

    First test passsed

--- pair-exercise/java/src/main/java/com/techelevator/KataStringCalculator.java
+++ pair-exercise/java/src/main/java/com/techelevator/KataStringCalculator.java
@@ -2,4 +2,15 @@ package com.techelevator;
 
 public class KataStringCalculator {
 
+	public static Object addString(String calc) {
+		// TODO Auto-generated method stub
+		calc = "";
+		
+		if (calc.equals("")) {
+			return 0;
+			}
+		return 0;
 }
+}
+
+
