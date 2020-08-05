c18998b0d8ffa874b1beae2cd0891a600c34331f
Steve Boese <steve.f.boese@gmail.com>
2020-08-05 15:41:48 -0400

    adding Scanners to search

--- pair-exercise-3/java/src/main/java/com/techelevator/FindAndReplace.java
+++ pair-exercise-3/java/src/main/java/com/techelevator/FindAndReplace.java
@@ -16,7 +16,17 @@ public class FindAndReplace {
 		if (!originalFile.exists()) {
 			System.out.println(originalFile.getAbsolutePath() + " does not exist.");
 		} else {
+			
+			Scanner searchWord = new Scanner(System.in);
+			System.out.println("Please enter a word to search: ");
+			String userWord = searchWord.nextLine();
+			
+			Scanner replaceWord = new Scanner(System.in);
+			System.out.println("Please enter a word to replace it with: ");
+			String userReplace = searchWord.nextLine();
+			
+			}
 		}
 	}
 
-}
+
