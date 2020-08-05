9b480ce064bc27ceab1cddc6bb2e386eca4d5dd0
Brad <bsller74@gmail.com>
2020-08-05 15:33:08 -0400

    First scanner

--- pair-exercise-3/java/src/main/java/com/techelevator/FindAndReplace.java
+++ pair-exercise-3/java/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,9 +1,22 @@
 package com.techelevator;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class FindAndReplace {
 
 	public static void main(String[] args) {
 
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Please enter the file path>>>>");
+		String path = userInput.nextLine();
+		
+		File originalFile = new File(path);
+		
+		if (!originalFile.exists()) {
+			System.out.println(originalFile.getAbsolutePath() + " does not exist.");
+		} else {
+		}
 	}
 
 }
